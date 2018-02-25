3a8853741bfdb1c3bb6938dd82dbf74a9ce42b1b
dermc <dermc@outlook.com>
2018-02-25 23:30:05 +0000

    Add project files.

---
+++ Pivotal_WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pivotal_WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+        public List<my_region> All_regions { get => all_regions; set => all_regions = value; }
+
+        private List<my_region> all_regions;
+
+        public List<my_region> List_all_Regions()
+        {
+            DateTime localDate = DateTime.Now;
+
+            List<my_region> region_list_populated = new List<my_region>();
+
+            region_list_populated.Add(new my_region("San Francisco", "en-US", -8));
+            region_list_populated.Add(new my_region("New York", "en-US", -5));
+            region_list_populated.Add(new my_region("London", "en-GB", 0));
+            // region_list_populated.Add(new my_region("Dublin", "en-GB", 0));
+            // region_list_populated.Add(new my_region("Singapore", "en-SG", +8));
+
+            return region_list_populated;
+        }
+
+
+        public void OnGet()
+        {
+            Message = "hello";
+
+            All_regions = List_all_Regions();
+        }
+    }
+}
+
---
+++ Pivotal_WebApp/my_region.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Pivotal_WebApp.Pages
+{
+    public class my_region
+    {
+        public string location;
+        public string culture;
+        public string local_time_localised;
+        public string status;
+
+        public my_region(string loc, string cul, int adj)
+        {
+            DateTime region_local_time = DateTime.Now.AddHours(adj);
+
+            this.location = loc;
+            this.culture = cul;
+            this.local_time_localised = region_local_time.ToString(new CultureInfo(cul));
+
+            this.status = "Unknown";
+
+            if (region_local_time.Hour >= 9 && region_local_time.Hour < 18)
+            {
+                this.status = "Office Open";
+            }
+            else
+            {
+                this.status = "* Outside working hours *";
+            }
+
+        }
+    }
+}
